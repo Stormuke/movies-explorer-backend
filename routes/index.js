95724530e0b567ec98691af8bc98abcb60219848
Stormuke <fen1x@yandex.ru>
2022-03-02 23:36:02 +0300

    fix: rout

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
+const loginRouter = require('./login');
 const userRouter = require('./users');
 const moviesRouter = require('./movies');
 const auth = require('../middlewares/auth');
 const NotFound = require('../errors/NotFound');
-const loginRouter = require('./login');
 
-router.use(loginRouter);
+
+router.use('/', loginRouter);
 
 router.use(auth);
 
