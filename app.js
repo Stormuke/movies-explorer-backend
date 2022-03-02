cc30d498884b341ccd0bd8d8233c6998ae786535
Stormuke <fen1x@yandex.ru>
2022-03-03 00:22:34 +0300

    fix: env

--- app.js
+++ app.js
@@ -8,24 +8,29 @@ const limiter = require('./middlewares/rateLimiter');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const errorHandler = require('./middlewares/errors');
 const route = require('./routes/index');
+const login = require('./routes/login');
+const DB_ADDRES = require('./utils/config')
 
 const app = express();
-const { PORT = 3000, DB_URL } = process.env;
+const { PORT = 3000, DB_URL, NODE_ENV } = process.env;
 
 app.use(bodyParser.json());
 app.use(requestLogger);
+
+mongoose.connect(NODE_ENV === 'production' ? DB_URL : DB_ADDRES, () => {
+  console.log('Подключение успешно');
+});
+
 app.use(helmet());
 app.use(limiter);
 
+app.use(login)
 app.use(route);
 
 app.use(errorLogger);
 app.use(errors());
 app.use(errorHandler);
 
-mongoose.connect(DB_URL, () => {
-  console.log('Подключение успешно');
-});
 app.listen(PORT, () => {
   console.log(`Started on port ${PORT}`);
 });
--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,6 @@ const moviesRouter = require('./movies');
 const auth = require('../middlewares/auth');
 const NotFound = require('../errors/NotFound');
 
-
-router.use(loginRouter);
-
 router.use(auth);
 
 router.use('/users', userRouter);
